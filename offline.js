a47041e6a74092fbbc2bbd275c6ee6e330155568
61131114002 <43134332+61131114002@users.noreply.github.com>
2021-03-01 12:06:30 +0700

    1
    1

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1614574113,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/pop-sheet0.png",
+		"images/start-sheet0.png",
+		"images/bg-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
